60489fa9534e059421289146554051421975386d
tory <myhandstied@gmail.com>
2022-10-20 21:52:39 -0700

    if-else

---
+++ examples/example4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("put your name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Tory")
+{
+    Console.WriteLine("yaay! It's YOU");      
+}
+else
+{
+    Console.Write("wazzup ");
+    Console.WriteLine(username);
+}
