9495c309c8820269005d469870972fb347441225
tory <myhandstied@gmail.com>
2022-10-21 01:51:01 -0700

    task0

--- examples/HW/task0/Program.cs
+++ examples/HW/task0/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+//Например:
+//4 -> 16 
+//-3 -> 9 
+//-7 -> 49
+
+System.Console.Write("enter the number >"); // show a hint on the screen
+string inputString = Console.ReadLine();       //  this one is to get entered line from console
+int value = Convert.ToInt32(inputString);     //   преобразуем к целому типу
+
+int square = value * value;                 //     вычисляем квадрат числа
+System.Console.WriteLine(square);          //      выводим его на экран (commant + alt + f = текст форматировать)
