9bb3d336f73d51d2854a7ab18bb61e4b685a6195
tory <myhandstied@gmail.com>
2022-10-21 00:01:16 -0700

    last fix + pravki Ivana :)

--- examples/example6pyramid/Program.cs
+++ examples/example6pyramid/Program.cs
@@ -17,13 +17,13 @@ Console.SetCursorPosition(xc, yc);
 Console.WriteLine("*");
 
 
-int x = xa, y = xb;
+int x = xa, y = yb;
 
 int count = 0;
 
 while(count < 10000)
 {
-    int what = new Random().Next(0, 3); //[0;3] 0 1 2
+    int what = new Random().Next(0, 3); //[0;3) 0 1 2)
     if (what == 0)
     {
         x = (x + xa) / 2;
@@ -44,5 +44,6 @@ while(count < 10000)
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("*");
-    count +=1;
+    count++;
+    Console.SetCursorPosition(1, 31);
 }
