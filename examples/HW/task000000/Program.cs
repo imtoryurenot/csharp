04cba3846133751513e6155f48fbbcc66d958524
tory <myhandstied@gmail.com>
2022-10-21 23:23:41 -0700

    added Task000000

---
+++ examples/HW/task000000/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+//1 -> 1
+//2 -> 0.5
+//0.25 -> 4
+
+System.Console.WriteLine("Enter the number");
+string inputString = Console.ReadLine();
+double Number = Convert.ToDouble(inputString );
+double ReversNumber= 1/Number;
+System.Console.WriteLine($"{ReversNumber:F2}"); // - вместо вот этого System.Console.WriteLine(ReversNumber); 
+// формат вывода, округление. число после запятой будет иметь два знака
+
