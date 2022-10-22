57b5673f8dc269dc4cccdb0f6b26082f09ba77f8
tory <myhandstied@gmail.com>
2022-10-22 02:06:59 -0700

    hwTask000

--- examples/HW/hwTask000/Program.cs
+++ examples/HW/hwTask000/Program.cs
@@ -3,4 +3,16 @@
 //-3 -> нет
 //7 -> нет
 
-Console.WriteLine("enter your first number :");
+Console.WriteLine("this programm is gonna help you to fint out if the number is divided by 2, enter your number>");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a %2 == 0) // до этого стоял / и почему-то программа работала некорректно. странно
+{
+    System.Console.WriteLine("it can be divided");
+}
+
+else
+{
+    System.Console.WriteLine("it can not be divided. choose another one :)");
+}
