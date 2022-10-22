1b722eba9a8e5a9a6f0a59bd4378458a23b7d13e
tory <myhandstied@gmail.com>
2022-10-21 20:48:32 -0700

    completed hwTask0

--- examples/HW/hwTask0/Program.cs
+++ examples/HW/hwTask0/Program.cs
@@ -2,3 +2,23 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+
+System.Console.WriteLine("enter the first number:");
+
+string number1 = Console.ReadLine();
+int a = Convert.ToInt32(number1);
+
+System.Console.WriteLine("enter the second number:");
+
+string number2 = Console.ReadLine();
+int b = Convert.ToInt32(number2);
+
+if (a > b)
+{
+    System.Console.WriteLine("fist one is max");
+}
+
+if (a < b);
+{
+    System.Console.WriteLine("second one is max");
+}
