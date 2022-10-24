dcbfd168c20a50048cb702d0db2b516796e90ba1
tory <myhandstied@gmail.com>
2022-10-24 02:32:50 -0700

    added Task0000

---
+++ examplesl2/HW2/Task0000/Program.cs
@@ -0,0 +1,25 @@
+﻿// напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+// 46 -> нет
+// 161 -> да
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+    bool DivBy (int number)
+    {
+        return (number % 7 == 0) && (number % 23 == 0); // поверили делится ли на 7 без остатка и делится ли на 23 без остатка с помощью == 0
+    }
+
+    int number = Prompt("enter your number:");
+   if (DivBy (number))
+   {
+    Console.WriteLine("yes");
+   } 
+   else
+   {
+    Console.WriteLine("no :c");
+   }
