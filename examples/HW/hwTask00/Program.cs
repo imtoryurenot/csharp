85dbd535dc144a5218520bb65798d1f2178647ce
tory <myhandstied@gmail.com>
2022-10-23 21:22:30 -0700

    changed hwTask00

--- examples/HW/hwTask00/Program.cs
+++ examples/HW/hwTask00/Program.cs
@@ -3,21 +3,47 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
-System.Console.WriteLine("Hi! this programm is gonna help you to find max of those 3 numbers that you are gonna enter here, so please enter the first number:");
+//System.Console.WriteLine("Hi! this programm is gonna help you to find max of those 3 numbers that you are gonna enter here, so please enter the first number:");
 
-int a = Convert.ToInt32(Console.ReadLine());
+////int a = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine("enter the second number:");
+//System.Console.WriteLine("enter the second number:"); 
 
-int b = Convert.ToInt32(Console.ReadLine());
+//int b = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine("enter the third number:");
+//System.Console.WriteLine("enter the third number:");
 
-int c = Convert.ToInt32(Console.ReadLine());
+//int c = Convert.ToInt32(Console.ReadLine());
 
-int[] nums = {a, b, c};
+//int[] nums = {a, b, c};
 
-int findmax(){
-    return nums.Max();
+///int findmax(){
+// return nums.Max();
+//}
+//System.Console.WriteLine($"thats the biggest one is {findmax()}");
+
+
+// АЛТЕРНАТИВНЫЙ ВАРИАНТ ТОГО ЖЕ КОДА
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
 }
-System.Console.WriteLine($"thats the biggest one is {findmax()}");
+
+int numberA = Prompt("Hi! this programm is gonna help you to find max of those 3 numbers that you are gonna enter here, so please enter the first number");
+int numberB = Prompt("enter the second number:");
+int numberC = Prompt("enter the third number:");
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+System.Console.WriteLine($"thats the biggest one is {max}");
