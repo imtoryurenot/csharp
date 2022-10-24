6612c877ab7126d9d2366baca8a6be51c99ed9ee
tory <myhandstied@gmail.com>
2022-10-24 01:52:01 -0700

    Task000

---
+++ examplesl2/HW2/Task000/Program.cs
@@ -0,0 +1,27 @@
+﻿// напишите программу, которая на вход будет принимать два числа
+// и выводить, является ли второе число кратным первому. если число 2 не кратно числу 1,
+// то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+    int DivBy (int B, int A) // вычисляем остаток от деления В на А (2 / 4 = 0 целых и 2 в остатке)
+    {
+        return (B % A);
+    }
+       int A = Prompt("enter first number: ");
+       int B = Prompt ("enter second number: ");
+
+       if (DivBy (A, B) == 0)
+       {
+        Console.WriteLine ("tuple"); // кратно в мире IT
+       }
+else 
+{
+    Console.WriteLine (DivBy(A, B));
+}
