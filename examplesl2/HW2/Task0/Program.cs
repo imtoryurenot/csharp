30dd663fa540d5060e1951b7404dbd0f77401882
tory <myhandstied@gmail.com>
2022-10-24 00:34:39 -0700

    added Task00

---
+++ examplesl2/HW2/Task0/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число, 
+//а на выходе показывет показывает последнюю цифру этого числа.
+
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100 || number >= 1000)
+    {
+        Console.WriteLine("this number does not contain three digits");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("enter a three digit number: ");
+if (ValidateNumber(number))
+{
+    int lastDigit = number % 10;
+    number = Prompt($"last digit of {number} is {lastDigit}:");
+}
---
+++ examplesl2/HW2/Task00/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = new Random().Next(10, 100);
+
+    int lastDigit = num / 10;
+    int firstDigit = num % 10;
+    
+        if (firstDigit > lastDigit || firstDigit < lastDigit)
+        {
+        System.Console.WriteLine($"{firstDigit} is the biggest digit of this random {num}");
+        }
+else
+    {
+    System.Console.WriteLine($"{lastDigit} is the biggest digit of this random {num}");
+    }
